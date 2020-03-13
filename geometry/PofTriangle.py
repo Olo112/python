657a82ec88c5e127d9c286d4149f40a1ef6c4e9b
Olo112 <60978252+Olo112@users.noreply.github.com>
2020-03-13 20:59:37 +0100

    Add files via upload

---
+++ geometry/PofTriangle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+
+half = (0.5)
+a = input("Enter the number of a...")
+aT = int(a)
+
+h = input("Now enter the number of h...")
+hT = int(h)
+
+PT = half * aT * hT
+
+print("Your answer is", int(PT))
+
+input("Press enter to exit...")
