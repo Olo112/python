fb1efd0972bc4ae242d130ec49c6925d71459e58
Oliwier Szymanski <60978252+Olo112@users.noreply.github.com>
2020-03-14 22:16:54 +0100

    Add files via upload

---
+++ geometry/PofSquare.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+
+a = input("Enter the number of a...")
+a2 = int(a)
+
+b = input("Now enter the number of b...")
+b2 = int(b)
+
+P = a2 * b2
+
+print("Your answer is:", int(P))
+
+input()
+
+
+
