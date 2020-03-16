d3eab486f62ae9ca1429362824484d7d17b7a754
Oliwier Szymanski <60978252+Olo112@users.noreply.github.com>
2020-03-16 11:44:56 +0100

    Add files via upload

---
+++ geometry/Pythagoras_theorem.py
@@ -0,0 +1,20 @@
+#===========================================================
+# ALGO: SQRT(a^2 + b^2)
+#===========================================================
+
+# sec. 1 - import(s) and asigning a constant value
+import math
+sqrt = math.sqrt
+
+# sec. 2 - main function
+def pyth(a, b):
+    c = sqrt(a**2 + b**2)
+    print("c =", c)
+
+# sec. 3 - test
+print("Test for a = 8 and b = 6")
+a = 8
+b = 6
+pyth(a, b)
+
+#======================== END OF FILE ======================
