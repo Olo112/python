ca73437ade20c5821d1bfc7bcaf80cf00765a2b0
Oliwier Szymanski <60978252+Olo112@users.noreply.github.com>
2020-03-17 17:51:10 +0100

    Add files via upload

---
+++ geometry/PofSphere.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+
+PI = 3.1415
+
+r = input("Please enter number of r...")
+rC = int(r)
+
+answer = 4 * PI * rC ** 3
+print("Your answer is", int(answer))
+
+input("Press enter to finish...")
